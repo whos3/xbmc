94ace74887f6f2c784bcbf825580ba43085698fb
montellese <montellese@xbmc.org>
2013-10-10 19:06:29 +0200

    [CFileItem] add IsEnabled() and SetEnabled()

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -6424,6 +6424,14 @@ const infomap container_str[]  = {{ "property",         CONTAINER_PROPERTY },
 ///     @skinning_v19 **[New Infolabel]** \link ListItem_TvShowDBID `ListItem.TvShowDBID`\endlink
 ///     <p>
 ///   }
+///   \table_row3{   <b>`ListItem.IsEnabled`</b>,
+///                  \anchor ListItem_IsEnabled
+///                  _boolean_,
+///     @return **True** if the item is enabled.
+///     <p><hr>
+///     @skinning_v19 **[New Infolabel]** \link ListItem_IsEnabled `ListItem.IsEnabled`\endlink
+///     <p>
+///   }
 /// \table_end
 ///
 /// -----------------------------------------------------------------------------
@@ -6631,6 +6639,7 @@ const infomap listitem_labels[]= {{ "thumb",            LISTITEM_THUMB },
                                   { "isfinale",         LISTITEM_IS_FINALE },
                                   { "islive",           LISTITEM_IS_LIVE },
                                   { "tvshowdbid",       LISTITEM_TVSHOWDBID },
+                                  { "isenabled",        LISTITEM_ISENABLED },
 };
 
 /// \page modules__infolabels_boolean_conditions
@@ -10683,6 +10692,15 @@ bool CGUIInfoManager::GetItemBool(const CGUIListItem *item, int contextWindow, i
         {
           const CFileItem *pItem = static_cast<const CFileItem *>(item);
           return pItem->IsParentFolder();
+        }
+        break;
+      }
+      case LISTITEM_ISENABLED:
+      {
+        if (item->IsFileItem())
+        {
+          const CFileItem *pItem = static_cast<const CFileItem *>(item);
+          return pItem->IsEnabled();
         }
         break;
       }
