2ea3b28cb5d50ca86e570d105cc67357dcd1aa34
montellese <montellese@xbmc.org>
2013-12-23 00:37:06 +0100

    CFileItemList: add copy constructor

--- xbmc/FileItem.h
+++ xbmc/FileItem.h
@@ -589,6 +589,7 @@ public:
 
   CFileItemList();
   explicit CFileItemList(const std::string& strPath);
+  CFileItemList(const CFileItemList &other);
   virtual ~CFileItemList();
   virtual void Archive(CArchive& ar);
   CFileItemPtr operator[] (int iItem);
