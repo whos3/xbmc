c993c3e3dc9e818b65cce1c64eabd8ba0c7b775b
montellese <montellese@xbmc.org>
2014-04-06 19:29:54 +0200

    video library: forward updates to playcount/lastplayed/resumepoint to CMediaImportManager

--- xbmc/utils/SaveFileStateJob.cpp
+++ xbmc/utils/SaveFileStateJob.cpp
@@ -33,6 +33,7 @@
 #include "guilib/GUIMessage.h"
 #include "guilib/GUIWindowManager.h"
 #include "GUIUserMessages.h"
+#include "media/import/MediaImportManager.h"
 #include "music/MusicDatabase.h"
 #include "cores/AudioEngine/DSPAddons/ActiveAEDSP.h"
 
@@ -54,7 +55,8 @@ bool CSaveFileStateJob::DoWork()
   {
 #ifdef HAS_UPNP
     // checks if UPnP server of this file is available and supports updating
-    if (URIUtils::IsUPnP(progressTrackingFile)
+    if (!m_item.IsImported() &&
+        URIUtils::IsUPnP(progressTrackingFile)
         && UPNP::CUPnP::SaveFileState(m_item, m_bookmark, m_updatePlayCount)) {
       return true;
     }
@@ -91,7 +93,10 @@ bool CSaveFileStateJob::DoWork()
             updateListing = true;
           }
           else
+          {
+            m_item.GetVideoInfoTag()->m_lastPlayed = CDateTime::GetCurrentDateTime();
             videodatabase.UpdateLastPlayed(m_item);
+          }
 
           if (!m_item.HasVideoInfoTag() || m_item.GetVideoInfoTag()->m_resumePoint.timeInSeconds != m_bookmark.timeInSeconds)
           {
@@ -214,6 +219,9 @@ bool CSaveFileStateJob::DoWork()
         audiodatabase.Close();
       }
     }
+
+    if (m_item.IsImported())
+      CMediaImportManager::GetInstance().UpdateImportedItem(m_item);
   }
   return true;
 }
