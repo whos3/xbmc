b25faec2011657341b1430ac3e7c42692dda1ffc
Garrett Brown <themagnificentmrb@gmail.com>
2015-02-18 17:30:52 -0800

    (0005) - Controller add-ons

--- xbmc/addons/AddonBuilder.cpp
+++ xbmc/addons/AddonBuilder.cpp
@@ -33,6 +33,7 @@
 #include "addons/Visualisation.h"
 #include "addons/Webinterface.h"
 #include "cores/AudioEngine/DSPAddons/ActiveAEDSPAddon.h"
+#include "games/controllers/Controller.h"
 #include "peripherals/addons/PeripheralAddon.h"
 #include "pvr/addons/PVRClient.h"
 
@@ -149,6 +150,8 @@ std::shared_ptr<IAddon> CAddonBuilder::Build()
       return CRepository::FromExtension(std::move(m_props), m_extPoint);
     case ADDON_CONTEXT_ITEM:
       return CContextMenuAddon::FromExtension(std::move(m_props), m_extPoint);
+    case ADDON_GAME_CONTROLLER:
+      return GAME::CController::FromExtension(std::move(m_props), m_extPoint);
     default:
       break;
   }
@@ -213,6 +216,8 @@ AddonPtr CAddonBuilder::FromProps(AddonProps addonProps)
       return AddonPtr(new CContextMenuAddon(std::move(addonProps)));
     case ADDON_PERIPHERALDLL:
       return AddonPtr(new PERIPHERALS::CPeripheralAddon(std::move(addonProps), false, false)); // TODO
+    case ADDON_GAME_CONTROLLER:
+      return AddonPtr(new GAME::CController(std::move(addonProps)));
     default:
       break;
   }
