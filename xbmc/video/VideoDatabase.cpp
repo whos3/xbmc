e8d16da04503ce0ff1afb962eface96915409783
montellese <montellese@xbmc.org>
2015-04-18 19:37:02 +0200

    [videodb] add possibility to set the parent path through AddFile()

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -896,7 +896,7 @@ bool CVideoDatabase::GetSourcePath(const std::string &path, std::string &sourceP
 }
 
 //********************************************************************************************************************************
-int CVideoDatabase::AddFile(const std::string& strFileNameAndPath)
+int CVideoDatabase::AddFile(const std::string& strFileNameAndPath, const std::string &parentPath /* = "" */)
 {
   std::string strSQL = "";
   try
@@ -910,7 +910,7 @@ int CVideoDatabase::AddFile(const std::string& strFileNameAndPath)
     std::string strFileName, strPath;
     SplitPath(strFileNameAndPath,strPath,strFileName);
 
-    int idPath = AddPath(strPath);
+    int idPath = AddPath(strPath, parentPath);
     if (idPath < 0)
       return -1;
 
