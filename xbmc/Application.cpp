notspiff/kodi-cmake

refactor CVideoLibraryQueue into CLibraryQueue

xbmc/Application.cpp
@@ -26,6 +26,7 @@
 #include "utils/Variant.h"
 #include "utils/Splash.h"
 #include "LangInfo.h"
+#include "LibraryQueue.h"
 #include "utils/Screenshot.h"
 #include "Util.h"
 #include "URL.h"
@@ -37,7 +38,6 @@
 #include "PlayListPlayer.h"
 #include "Autorun.h"
 #include "video/Bookmark.h"
-#include "video/VideoLibraryQueue.h"
 #include "network/NetworkServices.h"
 #include "guilib/GUIControlProfiler.h"
 #include "utils/LangCodeExpander.h"
@@ -2622,8 +2622,8 @@ void CApplication::Stop(int exitCode)
     if (m_musicInfoScanner->IsScanning())
       m_musicInfoScanner->Stop();
 
-    if (CVideoLibraryQueue::Get().IsRunning())
-      CVideoLibraryQueue::Get().CancelAllJobs();
+    if (CLibraryQueue::Get().IsRunning())
+      CLibraryQueue::Get().CancelAllJobs();
 
     CApplicationMessenger::Get().Cleanup();
 
@@ -3825,7 +3825,7 @@ void CApplication::CheckShutdown()
   if (m_bInhibitIdleShutdown
       || m_pPlayer->IsPlaying() || m_pPlayer->IsPausedPlayback() // is something playing?
       || m_musicInfoScanner->IsScanning()
-      || CVideoLibraryQueue::Get().IsRunning()
+      || CLibraryQueue::Get().IsRunning()
       || g_windowManager.IsWindowActive(WINDOW_DIALOG_PROGRESS) // progress dialog is onscreen
       || !g_PVRManager.CanSystemPowerdown(false))
   {
@@ -4721,7 +4721,7 @@ void CApplication::UpdateLibraries()
 
 bool CApplication::IsVideoScanning() const
 {
-  return CVideoLibraryQueue::Get().IsScanningLibrary();
+  return CLibraryQueue::Get().IsScanningLibrary();
 }
 
 bool CApplication::IsMusicScanning() const
@@ -4731,7 +4731,7 @@ bool CApplication::IsMusicScanning() const
 
 void CApplication::StopVideoScan()
 {
-  CVideoLibraryQueue::Get().StopLibraryScanning();
+  CLibraryQueue::Get().StopLibraryScanning();
 }
 
 void CApplication::StopMusicScan()
@@ -4742,19 +4742,19 @@ void CApplication::StopMusicScan()
 
 void CApplication::StartVideoCleanup(bool userInitiated /* = true */)
 {
-  if (userInitiated && CVideoLibraryQueue::Get().IsRunning())
+  if (userInitiated && CLibraryQueue::Get().IsRunning())
     return;
 
   std::set<int> paths;
   if (userInitiated)
-    CVideoLibraryQueue::Get().CleanLibraryModal(paths);
+    CLibraryQueue::Get().CleanVideoLibraryModal(paths);
   else
-    CVideoLibraryQueue::Get().CleanLibrary(paths, true);
+    CLibraryQueue::Get().CleanVideoLibrary(paths, true);
 }
 
 void CApplication::StartVideoScan(const std::string &strDirectory, bool userInitiated /* = true */, bool scanAll /* = false */)
 {
-  CVideoLibraryQueue::Get().ScanLibrary(strDirectory, scanAll, userInitiated);
+  CLibraryQueue::Get().ScanVideoLibrary(strDirectory, scanAll, userInitiated);
 }
 
 void CApplication::StartMusicCleanup(bool userInitiated /* = true */)

xbmc/LibraryQueue.h
@@ -27,32 +27,59 @@
 #include "utils/JobManager.h"
 
 class CGUIDialogProgressBarHandle;
-class CVideoLibraryJob;
 
 /*!
- \brief Queue for video library jobs.
+\brief Basic implementation/interface of a CJob which interacts with a library.
+*/
+class CLibraryJob : public CJob
+{
+public:
+  virtual ~CLibraryJob() { }
+
+  /*!
+  \brief Whether the job can be cancelled or not.
+  */
+  virtual bool CanBeCancelled() const { return false; }
+
+  /*!
+  \brief Tries to cancel the running job.
+
+  \return True if the job was cancelled, false otherwise
+  */
+  virtual bool Cancel() { return false; }
+
+  // implementation of CJob
+  virtual const char *GetType() const { return "LibraryJob"; }
+  virtual bool operator==(const CJob* job) const { return false; }
+
+protected:
+  CLibraryJob() { }
+};
+
+/*!
+ \brief Queue for library jobs.
 
  The queue can only process a single job at any time and every job will be
  executed at the lowest priority.
  */
-class CVideoLibraryQueue : protected CJobQueue
+class CLibraryQueue : protected CJobQueue
 {
 public:
-  ~CVideoLibraryQueue();
+  ~CLibraryQueue();
 
   /*!
-   \brief Gets the singleton instance of the video library queue.
+   \brief Gets the singleton instance of the library queue.
   */
-  static CVideoLibraryQueue& Get();
+  static CLibraryQueue& Get();
 
   /*!
-   \brief Enqueue a library scan job.
+   \brief Enqueue a video library scan job.
 
    \param[in] directory Directory to scan
    \param[in] scanAll Ignore exclude setting for items. Defaults to false
    \param[in] showProgress Whether or not to show a progress dialog. Defaults to true
    */
-  void ScanLibrary(const std::string& directory, bool scanAll = false, bool showProgress = true);
+  void ScanVideoLibrary(const std::string& directory, bool scanAll = false, bool showProgress = true);
 
   /*!
    \brief Check if a library scan is in progress.
@@ -67,20 +94,20 @@ public:
   void StopLibraryScanning();
 
   /*!
-   \brief Enqueue a library cleaning job.
+   \brief Enqueue a video library cleaning job.
 
    \param[in] paths Set with database IDs of paths to be cleaned
    \param[in] asynchronous Run the clean job asynchronously. Defaults to true
    \param[in] progressBar Progress bar to update in GUI. Defaults to NULL (no progress bar to update)
    */
-  void CleanLibrary(const std::set<int>& paths = std::set<int>(), bool asynchronous = true, CGUIDialogProgressBarHandle* progressBar = NULL);
+  void CleanVideoLibrary(const std::set<int>& paths = std::set<int>(), bool asynchronous = true, CGUIDialogProgressBarHandle* progressBar = NULL);
 
   /*!
-  \brief Executes a library cleaning with a modal dialog.
+  \brief Executes a video library cleaning with a modal dialog.
 
   \param[in] paths Set with database IDs of paths to be cleaned
   */
-  void CleanLibraryModal(const std::set<int>& paths = std::set<int>());
+  void CleanVideoLibraryModal(const std::set<int>& paths = std::set<int>());
 
   /*!
    \brief Queue a watched status update job.
@@ -93,16 +120,16 @@ public:
   /*!
    \brief Adds the given job to the queue.
 
-   \param[in] job Video library job to be queued.
+   \param[in] job Library job to be queued.
    */
-  void AddJob(CVideoLibraryJob *job);
+  void AddJob(CLibraryJob* job);
 
   /*!
    \brief Cancels the given job and removes it from the queue.
 
-   \param[in] job Video library job to be canceld and removed from the queue.
+   \param[in] job Library job to be canceld and removed from the queue.
    */
-  void CancelJob(CVideoLibraryJob *job);
+  void CancelJob(CLibraryJob* job);
 
   /*!
    \brief Cancels all running and queued jobs.
@@ -116,7 +143,7 @@ public:
 
 protected:
   // implementation of IJobCallback
-  virtual void OnJobComplete(unsigned int jobID, bool success, CJob *job);
+  virtual void OnJobComplete(unsigned int jobID, bool success, CJob* job);
 
   /*!
    \brief Notifies all to refresh the current listings.
@@ -124,13 +151,13 @@ protected:
   void Refresh();
 
 private:
-  CVideoLibraryQueue();
-  CVideoLibraryQueue(const CVideoLibraryQueue&);
-  CVideoLibraryQueue const& operator=(CVideoLibraryQueue const&);
+  CLibraryQueue();
+  CLibraryQueue(const CLibraryQueue&);
+  CLibraryQueue const& operator=(CLibraryQueue const&);
 
-  typedef std::set<CVideoLibraryJob*> VideoLibraryJobs;
-  typedef std::map<std::string, VideoLibraryJobs> VideoLibraryJobMap;
-  VideoLibraryJobMap m_jobs;
+  typedef std::set<CLibraryJob*> LibraryJobs;
+  typedef std::map<std::string, LibraryJobs> LibraryJobMap;
+  LibraryJobMap m_jobs;
   CCriticalSection m_critical;
 
   bool m_cleaning;

xbmc/video/VideoInfoScanner.cpp
@@ -31,6 +31,7 @@
 #include "filesystem/StackDirectory.h"
 #include "VideoInfoDownloader.h"
 #include "GUIInfoManager.h"
+#include "LibraryQueue.h"
 #include "filesystem/File.h"
 #include "dialogs/GUIDialogExtendedProgressBar.h"
 #include "dialogs/GUIDialogProgress.h"
@@ -46,7 +47,6 @@
 #include "utils/log.h"
 #include "utils/URIUtils.h"
 #include "utils/Variant.h"
-#include "video/VideoLibraryQueue.h"
 #include "video/VideoThumbLoader.h"
 #include "TextureCache.h"
 #include "GUIUserMessages.h"
@@ -94,7 +94,7 @@ namespace VIDEO
       if (m_bClean && m_pathsToScan.empty())
       {
         std::set<int> paths;
-        CVideoLibraryQueue::Get().CleanLibrary(paths, false, m_handle);
+        CLibraryQueue::Get().CleanVideoLibrary(paths, false, m_handle);
 
         if (m_handle)
           m_handle->MarkFinished();
@@ -150,7 +150,7 @@ namespace VIDEO
       if (!bCancelled)
       {
         if (m_bClean)
-          CVideoLibraryQueue::Get().CleanLibrary(m_pathsToClean, false, m_handle);
+          CLibraryQueue::Get().CleanVideoLibrary(m_pathsToClean, false, m_handle);
         else
         {
           if (m_handle)

xbmc/video/dialogs/GUIDialogVideoInfo.cpp
@@ -51,7 +51,7 @@
 #include "TextureCache.h"
 #include "music/MusicDatabase.h"
 #include "URL.h"
-#include "video/VideoLibraryQueue.h"
+#include "LibraryQueue.h"
 #include "video/VideoThumbLoader.h"
 #include "filesystem/Directory.h"
 #include "filesystem/VideoDatabaseDirectory.h"
@@ -1063,7 +1063,7 @@ int CGUIDialogVideoInfo::ManageVideoItem(const CFileItemPtr &item)
 
       case CONTEXT_BUTTON_MARK_WATCHED:
       case CONTEXT_BUTTON_MARK_UNWATCHED:
-        CVideoLibraryQueue::Get().MarkAsWatched(item, (button == (CONTEXT_BUTTON)CONTEXT_BUTTON_MARK_WATCHED));
+        CLibraryQueue::Get().MarkAsWatched(item, (button == (CONTEXT_BUTTON)CONTEXT_BUTTON_MARK_WATCHED));
         result = true;
         break;
 

xbmc/windows/GUIMediaWindow.cpp
@@ -25,6 +25,7 @@
 #include "FileItemListModification.h"
 #include "GUIPassword.h"
 #include "GUIUserMessages.h"
+#include "LibraryQueue.h"
 #include "PartyModeManager.h"
 #include "PlayListPlayer.h"
 #include "URL.h"
@@ -70,7 +71,6 @@
 #include "utils/TimeUtils.h"
 #include "utils/URIUtils.h"
 #include "video/VideoInfoTag.h"
-#include "video/VideoLibraryQueue.h"
 #include "windows/GUIWindowFileManager.h"
 
 #define CONTROL_BTNVIEWASICONS       2
@@ -1572,7 +1572,7 @@ bool CGUIMediaWindow::OnContextButton(int itemNumber, CONTEXT_BUTTON button)
     {
       CFileItemPtr item = m_vecItems->Get(itemNumber);
       m_viewControl.SetSelectedItem(m_viewControl.GetSelectedItem() + 1);
-      CVideoLibraryQueue::Get().MarkAsWatched(item, (button == CONTEXT_BUTTON_MARK_WATCHED));
+      CLibraryQueue::Get().MarkAsWatched(item, (button == CONTEXT_BUTTON_MARK_WATCHED));
       return true;
     }
   case CONTEXT_BUTTON_ADD_FAVOURITE:

whos3/xbmc

xbmc/Application.cpp
@@ -28,6 +28,7 @@
 #include "utils/Variant.h"
 #include "utils/Splash.h"
 #include "LangInfo.h"
+#include "LibraryQueue.h"
 #include "utils/Screenshot.h"
 #include "Util.h"
 #include "URL.h"
@@ -40,7 +41,6 @@
 #include "PlayListPlayer.h"
 #include "Autorun.h"
 #include "video/Bookmark.h"
-#include "video/VideoLibraryQueue.h"
 #include "guilib/GUIControlProfiler.h"
 #include "utils/LangCodeExpander.h"
 #include "GUIInfoManager.h"
@@ -2912,8 +2912,8 @@ void CApplication::Stop(int exitCode)
     if (m_musicInfoScanner->IsScanning())
       m_musicInfoScanner->Stop(true);
 
-    if (CVideoLibraryQueue::GetInstance().IsRunning())
-      CVideoLibraryQueue::GetInstance().CancelAllJobs();
+    if (CLibraryQueue::GetInstance().IsRunning())
+      CLibraryQueue::GetInstance().CancelAllJobs();
 
     CApplicationMessenger::GetInstance().SendMsg(TMSG_SETAUDIODSPSTATE, ACTIVE_AE_DSP_STATE_OFF); // send a blocking message to deactivate AudioDSP engine
     CApplicationMessenger::GetInstance().Cleanup();
@@ -4129,7 +4129,7 @@ void CApplication::CheckShutdown()
   if (m_bInhibitIdleShutdown
       || m_pPlayer->IsPlaying() || m_pPlayer->IsPausedPlayback() // is something playing?
       || m_musicInfoScanner->IsScanning()
-      || CVideoLibraryQueue::GetInstance().IsRunning()
+      || CLibraryQueue::GetInstance().IsRunning()
       || g_windowManager.IsWindowActive(WINDOW_DIALOG_PROGRESS) // progress dialog is onscreen
       || !g_PVRManager.CanSystemPowerdown(false))
   {
@@ -5013,7 +5013,7 @@ void CApplication::UpdateLibraries()
 
 bool CApplication::IsVideoScanning() const
 {
-  return CVideoLibraryQueue::GetInstance().IsScanningLibrary();
+  return CLibraryQueue::GetInstance().IsScanningLibrary();
 }
 
 bool CApplication::IsMusicScanning() const
@@ -5023,7 +5023,7 @@ bool CApplication::IsMusicScanning() const
 
 void CApplication::StopVideoScan()
 {
-  CVideoLibraryQueue::GetInstance().StopLibraryScanning();
+  CLibraryQueue::GetInstance().StopLibraryScanning();
 }
 
 void CApplication::StopMusicScan()
@@ -5034,19 +5034,19 @@ void CApplication::StopMusicScan()
 
 void CApplication::StartVideoCleanup(bool userInitiated /* = true */)
 {
-  if (userInitiated && CVideoLibraryQueue::GetInstance().IsRunning())
+  if (userInitiated && CLibraryQueue::GetInstance().IsRunning())
     return;
 
   std::set<int> paths;
   if (userInitiated)
-    CVideoLibraryQueue::GetInstance().CleanLibraryModal(paths);
+    CLibraryQueue::GetInstance().CleanVideoLibraryModal(paths);
   else
-    CVideoLibraryQueue::GetInstance().CleanLibrary(paths, true);
+    CLibraryQueue::GetInstance().CleanVideoLibrary(paths, true);
 }
 
 void CApplication::StartVideoScan(const std::string &strDirectory, bool userInitiated /* = true */, bool scanAll /* = false */)
 {
-  CVideoLibraryQueue::GetInstance().ScanLibrary(strDirectory, scanAll, userInitiated);
+  CLibraryQueue::GetInstance().ScanVideoLibrary(strDirectory, scanAll, userInitiated);
 }
 
 void CApplication::StartMusicCleanup(bool userInitiated /* = true */)

xbmc/LibraryQueue.h
@@ -27,32 +27,59 @@
 #include "utils/JobManager.h"
 
 class CGUIDialogProgressBarHandle;
-class CVideoLibraryJob;
 
 /*!
- \brief Queue for video library jobs.
+\brief Basic implementation/interface of a CJob which interacts with a library.
+*/
+class CLibraryJob : public CJob
+{
+public:
+  virtual ~CLibraryJob() { }
+
+  /*!
+  \brief Whether the job can be cancelled or not.
+  */
+  virtual bool CanBeCancelled() const { return false; }
+
+  /*!
+  \brief Tries to cancel the running job.
+
+  \return True if the job was cancelled, false otherwise
+  */
+  virtual bool Cancel() { return false; }
+
+  // implementation of CJob
+  virtual const char *GetType() const { return "LibraryJob"; }
+  virtual bool operator==(const CJob* job) const { return false; }
+
+protected:
+  CLibraryJob() { }
+};
+
+/*!
+ \brief Queue for library jobs.
 
  The queue can only process a single job at any time and every job will be
  executed at the lowest priority.
  */
-class CVideoLibraryQueue : protected CJobQueue
+class CLibraryQueue : protected CJobQueue
 {
 public:
-  ~CVideoLibraryQueue();
+  ~CLibraryQueue();
 
   /*!
-   \brief Gets the singleton instance of the video library queue.
+   \brief Gets the singleton instance of the library queue.
   */
-  static CVideoLibraryQueue& GetInstance();
+  static CLibraryQueue& GetInstance();
 
   /*!
-   \brief Enqueue a library scan job.
+   \brief Enqueue a video library scan job.
 
    \param[in] directory Directory to scan
    \param[in] scanAll Ignore exclude setting for items. Defaults to false
    \param[in] showProgress Whether or not to show a progress dialog. Defaults to true
    */
-  void ScanLibrary(const std::string& directory, bool scanAll = false, bool showProgress = true);
+  void ScanVideoLibrary(const std::string& directory, bool scanAll = false, bool showProgress = true);
 
   /*!
    \brief Check if a library scan is in progress.
@@ -67,20 +94,20 @@ public:
   void StopLibraryScanning();
 
   /*!
-   \brief Enqueue a library cleaning job.
+   \brief Enqueue a video library cleaning job.
 
    \param[in] paths Set with database IDs of paths to be cleaned
    \param[in] asynchronous Run the clean job asynchronously. Defaults to true
    \param[in] progressBar Progress bar to update in GUI. Defaults to NULL (no progress bar to update)
    */
-  void CleanLibrary(const std::set<int>& paths = std::set<int>(), bool asynchronous = true, CGUIDialogProgressBarHandle* progressBar = NULL);
+  void CleanVideoLibrary(const std::set<int>& paths = std::set<int>(), bool asynchronous = true, CGUIDialogProgressBarHandle* progressBar = NULL);
 
   /*!
-  \brief Executes a library cleaning with a modal dialog.
+  \brief Executes a video library cleaning with a modal dialog.
 
   \param[in] paths Set with database IDs of paths to be cleaned
   */
-  void CleanLibraryModal(const std::set<int>& paths = std::set<int>());
+  void CleanVideoLibraryModal(const std::set<int>& paths = std::set<int>());
 
   /*!
    \brief Enqueues a job to refresh the details of the given item.
@@ -114,16 +141,16 @@ public:
   /*!
    \brief Adds the given job to the queue.
 
-   \param[in] job Video library job to be queued.
+   \param[in] job Library job to be queued.
    */
-  void AddJob(CVideoLibraryJob *job);
+  void AddJob(CLibraryJob* job);
 
   /*!
    \brief Cancels the given job and removes it from the queue.
 
-   \param[in] job Video library job to be canceld and removed from the queue.
+   \param[in] job Library job to be canceld and removed from the queue.
    */
-  void CancelJob(CVideoLibraryJob *job);
+  void CancelJob(CLibraryJob* job);
 
   /*!
    \brief Cancels all running and queued jobs.
@@ -137,7 +164,7 @@ public:
 
 protected:
   // implementation of IJobCallback
-  virtual void OnJobComplete(unsigned int jobID, bool success, CJob *job);
+  virtual void OnJobComplete(unsigned int jobID, bool success, CJob* job);
 
   /*!
    \brief Notifies all to refresh the current listings.
@@ -145,13 +172,13 @@ protected:
   void Refresh();
 
 private:
-  CVideoLibraryQueue();
-  CVideoLibraryQueue(const CVideoLibraryQueue&);
-  CVideoLibraryQueue const& operator=(CVideoLibraryQueue const&);
+  CLibraryQueue();
+  CLibraryQueue(const CLibraryQueue&);
+  CLibraryQueue const& operator=(CLibraryQueue const&);
 
-  typedef std::set<CVideoLibraryJob*> VideoLibraryJobs;
-  typedef std::map<std::string, VideoLibraryJobs> VideoLibraryJobMap;
-  VideoLibraryJobMap m_jobs;
+  typedef std::set<CLibraryJob*> LibraryJobs;
+  typedef std::map<std::string, LibraryJobs> LibraryJobMap;
+  LibraryJobMap m_jobs;
   CCriticalSection m_critical;
 
   bool m_modal;

xbmc/video/VideoInfoScanner.cpp
@@ -37,6 +37,7 @@
 #include "guilib/LocalizeStrings.h"
 #include "GUIUserMessages.h"
 #include "interfaces/AnnouncementManager.h"
+#include "LibraryQueue.h"
 #include "messaging/ApplicationMessenger.h"
 #include "messaging/helpers/DialogHelper.h"
 #include "NfoFile.h"
@@ -52,7 +53,6 @@
 #include "utils/StringUtils.h"
 #include "utils/URIUtils.h"
 #include "utils/Variant.h"
-#include "video/VideoLibraryQueue.h"
 #include "video/VideoThumbLoader.h"
 #include "VideoInfoDownloader.h"
 
@@ -100,7 +100,7 @@ namespace VIDEO
       if (m_bClean && m_pathsToScan.empty())
       {
         std::set<int> paths;
-        CVideoLibraryQueue::GetInstance().CleanLibrary(paths, false, m_handle);
+        CLibraryQueue::GetInstance().CleanVideoLibrary(paths, false, m_handle);
 
         if (m_handle)
           m_handle->MarkFinished();
@@ -156,7 +156,7 @@ namespace VIDEO
       if (!bCancelled)
       {
         if (m_bClean)
-          CVideoLibraryQueue::GetInstance().CleanLibrary(m_pathsToClean, false, m_handle);
+          CLibraryQueue::GetInstance().CleanVideoLibrary(m_pathsToClean, false, m_handle);
         else
         {
           if (m_handle)

xbmc/video/dialogs/GUIDialogVideoInfo.cpp
@@ -51,7 +51,7 @@
 #include "TextureCache.h"
 #include "music/MusicDatabase.h"
 #include "URL.h"
-#include "video/VideoLibraryQueue.h"
+#include "LibraryQueue.h"
 #include "video/VideoThumbLoader.h"
 #include "filesystem/Directory.h"
 #include "filesystem/VideoDatabaseDirectory.h"
@@ -1107,7 +1107,7 @@ int CGUIDialogVideoInfo::ManageVideoItem(const CFileItemPtr &item)
 
       case CONTEXT_BUTTON_MARK_WATCHED:
       case CONTEXT_BUTTON_MARK_UNWATCHED:
-        CVideoLibraryQueue::GetInstance().MarkAsWatched(item, (button == (CONTEXT_BUTTON)CONTEXT_BUTTON_MARK_WATCHED));
+        CLibraryQueue::GetInstance().MarkAsWatched(item, (button == (CONTEXT_BUTTON)CONTEXT_BUTTON_MARK_WATCHED));
         result = true;
         break;
 

xbmc/windows/GUIMediaWindow.cpp
@@ -25,6 +25,7 @@
 #include "FileItemListModification.h"
 #include "GUIPassword.h"
 #include "GUIUserMessages.h"
+#include "LibraryQueue.h"
 #include "PartyModeManager.h"
 #include "PlayListPlayer.h"
 #include "URL.h"
@@ -67,7 +68,6 @@
 #include "utils/StringUtils.h"
 #include "utils/URIUtils.h"
 #include "utils/Variant.h"
-#include "video/VideoLibraryQueue.h"
 
 #define CONTROL_BTNVIEWASICONS       2
 #define CONTROL_BTNSORTBY            3
@@ -1576,7 +1576,7 @@ bool CGUIMediaWindow::OnContextButton(int itemNumber, CONTEXT_BUTTON button)
     {
       CFileItemPtr item = m_vecItems->Get(itemNumber);
       m_viewControl.SetSelectedItem(m_viewControl.GetSelectedItem() + 1);
-      CVideoLibraryQueue::GetInstance().MarkAsWatched(item, (button == CONTEXT_BUTTON_MARK_WATCHED));
+      CLibraryQueue::GetInstance().MarkAsWatched(item, (button == CONTEXT_BUTTON_MARK_WATCHED));
       return true;
     }
   case CONTEXT_BUTTON_ADD_FAVOURITE: