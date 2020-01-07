f1b86ed1dfe5d32a6b2aeebef7d631d4b5ce4a25
montellese <montellese@kodi.tv>
2020-01-07 23:13:16 +0100

    [CAddonMediaImporter][legacy/ListItem] add getMediaProviderId() and getMediaImportPath()

--- xbmc/interfaces/legacy/ListItem.cpp
+++ xbmc/interfaces/legacy/ListItem.cpp
@@ -895,5 +895,15 @@ namespace XBMCAddon
     {
       return item->GetVideoInfoTag();
     }
+
+    String ListItem::getMediaProviderId() const
+    {
+      return item->GetSource();
+    }
+
+    String ListItem::getMediaImportPath() const
+    {
+      return item->GetImportPath();
+    }
   }
 }
